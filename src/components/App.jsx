8b697f84efa2ec155a2392a9af9e30b3968ce38c
Arbabian21 <arifarbab21@gmail.com>
2024-09-20 12:53:38 -0400

    added state to display submission

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+	const [inputText, setInputText] = useState();
+	const [isSubmitted, setIsSubmitted] = useState(false);
+
+	function handleChange(event) {
+		setInputText(event.target.value);
+	}
+
+	function handleSubmit() {
+		setIsSubmitted(true);
+	}
+
+	return (
+		<div className="container">
+			<h1>Hello {isSubmitted && inputText}</h1>
+			<input
+				type="text"
+				placeholder="What's your name?"
+				onChange={handleChange}
+				value={inputText}
+			/>
+			<button onClick={handleSubmit}>Submit</button>
+		</div>
+	);
 }
 
 export default App;
