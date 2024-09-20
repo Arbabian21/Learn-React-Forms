784d1237df5152105efa7a8c7f91b77a01b6f32d
Arbabian21 <arifarbab21@gmail.com>
2024-09-20 13:04:31 -0400

    handling state for Forms

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,26 +2,30 @@ import React, { useState } from "react";
 
 function App() {
 	const [inputText, setInputText] = useState();
-	const [isSubmitted, setIsSubmitted] = useState(false);
+	const [headingText, setHeadingText] = useState("");
 
 	function handleChange(event) {
 		setInputText(event.target.value);
 	}
 
-	function handleSubmit() {
-		setIsSubmitted(true);
+	function handleSubmit(event) {
+		setHeadingText(inputText);
+
+		event.preventDefault();
 	}
 
 	return (
 		<div className="container">
-			<h1>Hello {isSubmitted && inputText}</h1>
-			<input
-				type="text"
-				placeholder="What's your name?"
-				onChange={handleChange}
-				value={inputText}
-			/>
-			<button onClick={handleSubmit}>Submit</button>
+			<h1>Hello {headingText}</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="What's your name?"
+					onChange={handleChange}
+					value={inputText}
+				/>
+				<button type="submit">Submit</button>
+			</form>
 		</div>
 	);
 }
